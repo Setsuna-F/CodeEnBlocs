3bfdac380fae6c52f9ce8b85383ffd44d8592203
CaMarchPa <chouenyib@gmail.com>
2016-12-09 17:10:44 +0100

    absmodel_cpp implemented

--- src/model/absmodel.cpp
+++ src/model/absmodel.cpp
@@ -26,11 +26,31 @@ AbsModel::~AbsModel() {
 }
 
 sct_type AbsModel::execute() {
-	/*int operand = std::stoi( this->UnaryModel::getOperand()->execute() );
-
-	operand = operand >= 0 ? operand : operand * (-1);
-	return std::to_string(operand);*/
+	sct_type operand;
+	operand.int_type		=nullptr;
+	operand.double_type	    =nullptr;
+	operand.bool_type		=nullptr;
 	
-	sct_type res;
-	return res;
+	operand = this->UnaryModel::getOperand()->execute();
+	
+	/*Premiere operande int ou double*/
+	if(operand.int_type) {
+		std::fflush(stdout);
+		*operand.int_type >= 0 ? *operand.int_type : *operand.int_type * (-1);
+		operand.double_type	    =nullptr;
+		operand.bool_type		=nullptr;
+		return operand;
+	}
+	
+	else if(operand.double_type){
+		std::fflush(stdout);
+		operand.int_type		=nullptr;
+		*operand.double_type >= 0 ? *operand.double_type : *operand.double_type * (-1);
+		operand.bool_type		=nullptr;
+		return operand;
+	}
+
+	/*cas erreur*/
+	else
+		return operand;
 }

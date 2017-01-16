7255e4f996096bc412958c9c9555c2885e1d8c2d
Wagner Pascal <rascal_wagner@hotmail.fr>
2017-01-16 09:22:25 +0100

    Intégration de nouveaux niveaux dans l'arbre

--- src/model/gamemodel.cpp
+++ src/model/gamemodel.cpp
@@ -2,6 +2,9 @@
 #include "iolevelmodel.hpp"
 #include "additionlevelmodel.hpp"
 #include "additionslevelmodel.hpp"
+#include "sublevelmodel.hpp"
+#include "multlevelmodel.hpp"
+#include "divlevelmodel.hpp"
 
 using namespace satap;
 
@@ -9,6 +12,9 @@ GameModel::GameModel(){
 	_levelList.push_back(new IOLevelModel());
 	_levelList.push_back(new AdditionLevelModel());
 	_levelList.push_back(new AdditionsLevelModel());
+	_levelList.push_back(new SubLevelModel());
+	_levelList.push_back(new MultLevelModel());
+	_levelList.push_back(new DivLevelModel());
 }
 
 std::vector<LevelModel*> GameModel::getLevelList(){
